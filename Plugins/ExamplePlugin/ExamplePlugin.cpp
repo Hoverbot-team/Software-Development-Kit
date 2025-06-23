eccb7502e7c25823edc2c716bfed18686dc77d33
Michasssada <michaslate@gmail.com@gmail.com>
2025-06-23 23:18:13 +0100

    added example plugin and made everything work

---
+++ Plugins/ExamplePlugin/ExamplePlugin.cpp
@@ -0,0 +1,27 @@
+// MyMod.cpp
+#include "SDKInterface.hpp"
+
+#include <iostream>
+
+class MyMod : public IMod {
+public:
+    void onLoad() override {
+        std::cout << "MyMod loaded\n";
+    }
+
+    void onUpdate(float dt) override {
+        std::cout << "MyMod updating (" << dt << "s)\n";
+    }
+
+    std::string getName() const override {
+        return "MyMod";
+    }
+
+    std::string getVersion() const override {
+        return "1.0";
+    }
+};
+
+extern "C" IMod* createMod() {
+    return new MyMod();
+}
